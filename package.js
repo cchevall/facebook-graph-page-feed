30f911c67fc7559b48ea51f939469f83f007594f
cchevallay <cchevallay@technicis.fr>
2017-02-12 20:53:16 +0100

    version 0.1.17 link feed full picture

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cchevallay:facebook-graph-page-feed',
-  version: '0.1.16',
+  version: '0.1.17',
   summary: './README.md',
   git: 'https://github.com/cchevall/facebook-graph-page-feed.git',
   documentation: 'README.md'
