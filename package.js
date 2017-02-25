05ffcf3d29843812433679b177c0849df2af2ba0
cchevallay <cchevallay@technicis.fr>
2017-02-25 12:50:39 +0100

    version 0.1.20 fix http call serialize

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cchevallay:facebook-graph-page-feed',
-  version: '0.1.19',
+  version: '0.1.20',
   summary: './README.md',
   git: 'https://github.com/cchevall/facebook-graph-page-feed.git',
   documentation: 'README.md'
